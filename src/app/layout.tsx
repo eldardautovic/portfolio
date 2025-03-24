e2fcca9958a65e3c1adfbe07916b8cb333ddee6a
Eldar Dautović <eldarcoox123@gmail.com>
2025-03-24 21:08:43 +0100

    Implemented a Hero section.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,47 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
-import { NavBar } from "@/components/NavBar";
+import {NavBar} from "@/components/NavBar";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Eldar Dautović",
-  description: "A portfolio to showcase my skills -- Eldar Dautović",
+    title: "Eldar Dautović",
+    description: "A portfolio to showcase my skills -- Eldar Dautović",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en" className="dark">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         <div className="relative flex h-screen w-full bg-white dark:bg-slate-950">
-          <div className="absolute inset-0 [background-size:10px_10px] [background-image:linear-gradient(to_right,rgba(147,197,253,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(147,197,253,0.2)_1px,transparent_1px)] dark:[background-image:linear-gradient(to_right,rgba(6,95,70,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(6,95,70,0.2)_1px,transparent_1px)]" />
-          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_60%,black)] dark:bg-black"></div>{" "}
-          <div className="z-20 container mx-auto px-7 py-10">
-            <NavBar />
+            <div
+                className="absolute inset-0 [background-size:10px_10px] [background-image:linear-gradient(to_right,rgba(147,197,253,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(147,197,253,0.2)_1px,transparent_1px)] dark:[background-image:linear-gradient(to_right,rgba(6,95,70,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(6,95,70,0.2)_1px,transparent_1px)]"
+            />
 
-            {children}
-          </div>
+            <div
+                className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_60%,black)] dark:bg-black"
+            />
+
+            <div className="relative z-20 w-full max-w-6xl mx-auto px-7 py-10">
+                <NavBar/>
+                {children}
+            </div>
         </div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from "next/image";
+
+function Hero() {
+    return (
+        <div className="container mx-auto py-6 md:py-12">
+            <div
+                className="flex flex-col-reverse md:flex-row items-center justify-between space-y-6 md:space-y-0 md:space-x-8">
+                <div className="flex flex-col text-left w-full md:w-1/2">
+                    <h1 className="scroll-m-20 text-3xl md:text-4xl lg:text-5xl font-extrabold tracking-tight text-slate-100 mb-4">
+                        Eldar<span className="text-teal-300"> Dautović</span>
+                    </h1>
+                    <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-slate-300 mb-3">
+                        A Full-stack Web Developer
+                    </h3>
+                    <p className="leading-7 text-slate-400">
+                        Transforming ideas into scalable web applications with 4 years of expertise.
+                    </p>
+                </div>
+                <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+                    <div className="w-48 md:w-64 lg:w-80 aspect-square relative">
+                        <Image
+                            src="/assets/hero-image.webp"
+                            alt="Hero image -- Eldar Memoji"
+                            layout="fill"
+                            objectFit="contain"
+                            className="rounded-lg"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
