93b2d7bb0977964a37675062ec97e0554c57dac3
Eldar Dautović <eldarcoox123@gmail.com>
2025-03-27 17:12:04 +0100

    Modified header to be in descending order to follow best semantic practices.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,9 +10,9 @@ function Hero() {
                     <h1 className="scroll-m-20 text-3xl md:text-4xl lg:text-5xl font-extrabold tracking-tight text-slate-100 mb-4">
                         Eldar<span className="text-cyan-400"> Dautović</span>
                     </h1>
-                    <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-slate-300 mb-3">
+                    <h2 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-slate-300 mb-3">
                         A Full-stack Web Developer
-                    </h3>
+                    </h2>
                     <p className="leading-7 text-slate-400 text-md md:text-lg ">
                         Transforming ideas into scalable web applications with 4 years of expertise.
                     </p>
--- src/components/ui/section-header.tsx
+++ src/components/ui/section-header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SectionHeader({title}: { title: string }) {
     return (
         <div className="flex flex-col items-start">
-            <h5 className="uppercase scroll-m-20 text-sm md:text-md font-semibold tracking-normal pb-2 text-slate-400">{title}</h5>
+            <h3 className="uppercase scroll-m-20 text-sm md:text-md font-semibold tracking-normal pb-2 text-slate-400">{title}</h3>
             <div className="w-full bg-slate-600/60 h-px"></div>
         </div>
     );
